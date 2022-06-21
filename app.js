02b84fb8b5c9229745101a04133b1c4b6857d7e4
abhi2002dhi <abhi2002dhiman@gmail.com>
2022-06-21 14:49:06 +0530

    add script

--- app.js
+++ app.js
@@ -109,7 +109,10 @@ app.post('/bank/:senderId/transferMoney', async (req, res) => {
     
 })
 
-app.listen('3000', ()=>{
-    console.log("listening on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
 });
+
 
